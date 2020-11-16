fb0469027c0dbf692971e167df200aa862f7bcc2
nehalaswani <70094352+nehalaswani@users.noreply.github.com>
2020-11-16 23:36:39 +0530

    Update Color Detection in an image.py

--- Color Detection in an image.py
+++ Color Detection in an image.py
@@ -1,53 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # THE SPARK FOUNDATION
-# ## IOT AND COMPUTER VISION
-# ### TASK: Implement an image color detector which identifies all the colors in an image
-
-# Importing necessary libraries
-
-# In[1]:
-
-
 import cv2
 import numpy as np
 import pandas as pd
 from PIL import Image 
 
+Image.open('color.jpg') 
 
-# Image we are using for color detection
-
-# In[2]:
-
-
-Image.open('/Users/dell/Desktop/color1.jpg') 
-
-
-# Reading image using cv2
-
-# In[3]:
-
-
-img = cv2.imread('/Users/dell/Desktop/color1.jpg')
+img = cv2.imread('color.jpg')
 img=cv2.resize(img,(700,500))
 
 clicked = False
 r = g = b = xpos = ypos = 0
 
 
-# Reading csv file with pandas and giving names to each column
-
-# In[4]:
-
-
 index=["color","color_name","hex","R","G","B"]
 csv = pd.read_csv(r'C:\Users\dell\Desktop\colors.csv', names=index, header=None)
-
-
-# Function to calculate minimum distance from all colors and get the most matching color
-
-# In[5]:
 
 
 def getColorName(R,G,B):
@@ -59,12 +25,6 @@ def getColorName(R,G,B):
             cname = csv.loc[i,"color_name"]
     return cname 
 
-
-# Function to get x,y coordinates of mouse double click 
-
-# In[6]:
-
-
 def draw_function(event, x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         global b,g,r,xpos,ypos, clicked
@@ -75,10 +35,6 @@ def draw_function(event, x,y,flags,param):
         b = int(b)
         g = int(g)
         r = int(r)
-
-
-# In[7]:
-
 
 cv2.namedWindow('color detection')
 cv2.setMouseCallback('color detection',draw_function)
@@ -106,10 +62,3 @@ while(1):
         break
     
 cv2.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
-
